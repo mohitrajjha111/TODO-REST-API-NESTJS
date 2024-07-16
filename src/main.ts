7d856382584d208a12c7ebd3b0daec9d52560e96
mohitrajjha111 <mohitrajjha111@gmail.com>
2024-07-16 22:04:40 +0530

    vercel deployment

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+    .setTitle('Todo REST API')
+    .setDescription("A REST api to list user's todos/tasks, authorization implemented using JWT Token. The API is made using NestJS.")
+    .setVersion('1.0')
+    .addTag('todos')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
